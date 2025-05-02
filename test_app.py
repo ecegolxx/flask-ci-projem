2f172cb4307429c1c199dd152b6acf88a5449417
ecegolxx <ecegolx@outlook.com>
2025-05-02 03:27:48 +0300

    yazım hatası düzeldi

--- test_app.py
+++ test_app.py
@@ -4,4 +4,5 @@ def test_hello():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert "Merhaba Dunya!" in response.data.decode()
+    assert "Merhaba Dünya!" in response.data.decode()
+
